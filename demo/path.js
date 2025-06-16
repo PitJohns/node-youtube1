a8de9b2d63d9b0fff0353c3b55360898c8063830
Veniamin Evstratov <dzimmutenno@gmail.com>
2025-06-16 18:41:27 +0500

    19.12

---
+++ demo/path.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+console.log('Название файла: ', path.basename(__filename))
+
+console.log('Имя директории: ', path.dirname(__filename))
+
+console.log('Расширение файла: ', path.extname(__filename))
